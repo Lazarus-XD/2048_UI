c0c96117f0a696184e5fe1e05a7816e40b9de7c6
Rizwan Ahsan <ahsanm7@mcmaster.ca>
2021-01-16 22:48:48 -0500

    Created ComplexT class and added getters

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -1,5 +1,20 @@
 ## @file complex_adt.py
-#  @author 
-#  @brief 
+#  @author Rizwan Ahsan
+#  @brief
 #  @date 
 
+import math
+
+class ComplexT():
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+
+    def real(self):
+        return self.x
+
+    def imag(self):
+        return self.y
+
+    def get_r(self):
+        return math.sqrt(self.x**2 + self.y**2)
