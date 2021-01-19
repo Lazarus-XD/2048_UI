5ef71a1db8743efc60d1c3cdb04d55a207cddcca
Rizwan Ahsan <ahsanm7@mcmaster.ca>
2021-01-19 17:08:59 -0500

    added tri_type method and documentation

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -1,27 +1,44 @@
 ## @file complex_adt.py
 #  @author Rizwan Ahsan
-#  @brief
-#  @date 
+#  @brief Contains a class for working with complex numbers
+#  @date 01/19/2021
 
 import math
 
+## @brief An ADT class for representing complex numbers
+#  @details A complex number is composed of a real part and an imaginary part
 class ComplexT():
-    def __init__(self, x, y):
-        if (isinstance(x, int) or isinstance(x, float)) and (isinstance(y, int) or isinstance(y, float)):
-            self.__x = float(x)
-            self.__y = float(y)
-        else:
-            raise Exception("x and y must be float")
 
+    ## @brief Contructor for ComplexT class
+    #  @details Creates a complex number based on the provided arguments
+    #  @param x Float representing the real part of the complex number
+    #  @param y Float representing the imaginary part of the complex number
+    #  @throws TypeError Throws if the arguments are not float
+    def __init__(self, x, y):
+        if isinstance(x, float) and isinstance(y, float):
+            self.__x = x
+            self.__y = y
+        else:
+            raise TypeError("x and y must be float")
+
+    ## @brief Gets the real part of the complex number
+    #  @return Float representing the real part
     def real(self):
         return self.__x
 
+    ## @brief Gets the imaginary part of the complex number
+    #  @return Float representing the imaginary part
     def imag(self):
         return self.__y
 
+    ## @brief Gets the modulus of the complex number
+    #  @return Float representing the modulus
     def get_r(self):
         return math.sqrt(self.__x ** 2 + self.__y ** 2)
 
+    ## @brief Gets the argument of the complex number
+    #  @return Float representing the argument
+    #  @throws Exception Throws if both the real part and imaginary part is zero
     def get_phi(self):
         if self.__x > 0:
             return math.atan(self.__y / self.__x)
@@ -38,40 +55,66 @@ class ComplexT():
             else:
                 raise Exception("undefined answer")
 
+    ## @brief Checks if the provided complex number is equal to this complex number
+    #  @param complex_num ComplexT to check if it is equal to this one
+    #  @return True if they are equal else False
     def equal(self, complex_num):
         return self.real() == complex_num.real() and self.imag() == complex_num.imag()
 
+    ## @brief Checks if the provided complex number is equal to this complex number
+    #  @param other ComplexT to check if it is equal to this one
+    #  @return True if they are equal else False
     def __eq__(self, other):
         return self.equal(other)
 
+    ## @brief Gets the complex conjugate of the complex number
+    #  @return ComplexT representing the complex conjugate
     def conj(self):
         return ComplexT(self.__x, - self.__y)
 
+    ## @brief Adds the given complex number to this number
+    #  @param complex_num ComplexT number to add to this number
+    #  @return ComplexT which results from adding both the numbers
     def add(self, complex_num):
         return ComplexT(self.__x + complex_num.real(), self.__y + complex_num.imag())
 
+    ## @brief Subtracts the given complex number from this number
+    #  @param complex_num ComplexT number to subtract from this number
+    #  @return ComplexT which results from the subtraction
     def sub(self, complex_num):
         return ComplexT(self.__x - complex_num.real(), self.__y - complex_num.imag())
 
+    ## @brief Multiplies the given complex number and this number
+    #  @param complex_num ComplexT number to multiply to this number
+    #  @return ComplexT which results from the multiplication
     def mult(self, complex_num):
         x = self.__x * complex_num.real() - self.__y * complex_num.imag()
         y = self.__x * complex_num.imag() - self.__y * complex_num.real()
         return ComplexT(x, y)
 
+    ## @brief Gets the reciprocal of the complex number
+    #  @return ComplexT which is the reciprocal of this complex number
+    #  @throws Exception throws if the denom variable is zero
     def recip(self):
-        denom = self.__x ** 2 + self.__y ** 2 #denom cannot be zero
+        denom = self.__x ** 2 + self.__y ** 2
         if denom == 0:
             raise Exception("The denominator should be non-zero i.e. x^2 + y^2 must be greater than 0")
         return ComplexT(self.__x / denom, - self.__y / denom)
 
+    ## @brief Divides this number by the provided complex number
+    #  @param complex_num ComplexT number which is used to divide this number
+    #  @return ComplexT which results from the division of the numbers
+    #  @throws Exception throws if the denom varible is zero
     def div(self, complex_num):
-        denom = complex_num.real() ** 2 + complex_num.imag() ** 2 #denom cannot be zero
+        denom = complex_num.real() ** 2 + complex_num.imag() ** 2
         if denom == 0:
             raise Exception("The argument must be a non-zero complex number")
         x = self.__x * complex_num.real() + self.__y * complex_num.imag()
         y = self.__y * complex_num.real() - self.__x * complex_num.imag()
         return ComplexT(x / denom, y / denom)
 
+    ## @brief Gets the positive square root of the complex number
+    #  @return ComplexT which is the positive square root of the complex number
     def sqrt(self):
         r = self.get_r()
         theta = self.get_phi()
--- A1/src/triangle_adt.py
+++ A1/src/triangle_adt.py
@@ -1,36 +1,58 @@
 ## @file triangle_adt.py
 #  @author Rizwan Ahsan
-#  @brief 
-#  @date
+#  @brief Contains a class for working with triangles
+#  @date 01/19/2021
 
 import math
+from enum import Enum, auto
 
+## @brief An ADT class for representing triangles
+#  @details A triangle is composed of three sides
 class TriangleT():
+
+    ## @brief Contructor for TriangleT class
+    #  @details Creates a triangle based on the provided arguments
+    #  @param x Integer representing a side of the triangle
+    #  @param y Integer representing a side of the triangle
+    #  @param z Integer representing a side of the triangle
+    #  @throws Exception Throws if all arguments are not integer or if any of the sides
+    #  are less than or equal to zero
     def __init__(self, x, y, z):
         if isinstance(x, int) and isinstance(y, int) and isinstance(z, int):
+            if x <= 0 or y <= 0 or z <= 0:
+                raise Exception("Length of each sides must be greater than 0")
             self.__x = x
             self.__y = y
             self.__z = z
-        elif x <= 0 or y <= 0 or z <= 0:
-            raise Exception("Length of each sides must be greater than 0")
         else:
             raise Exception("All arguments must be of type integer")
 
+    ## @brief Gets the values of all the sides of the triangle
+    #  @return Tuple containing all the sides
     def get_sides(self):
         return (self.__x, self.__y, self.__z)
 
+    ## @brief Checks if the provided triangle is equal to this triangle
+    #  @param triangle The triangle which is used to compare the equality
+    #  @return True is both the triangles are equal, false otherwise
     def equal(self, triangle):
         sides1 = self.get_sides()
         sides2 = triangle.get_sides()
         return sides1[0] == sides2[0] and sides1[1] == sides2[1] and sides1[2] == sides2[2]
 
+    ## @brief Gets the perimeter of the triangle
+    #  @return Integer which represents the perimeter of the triangle
     def perim(self):
         return self.__z + self.__y + self.__x
 
+    ## @brief Gets the area of the traingle
+    #  @return Float representing the area of the triangle
     def area(self):
         half = self.perim() / 2
         return math.sqrt(half * (half - self.__x) * (half - self.__y) * (half - self.__z))
 
+    ## @brief Checks if the provided sides creates a valid triangle or not
+    #  @return True if the triangle is valid, false otherwise
     def is_valid(self):
         cond1 = self.__x + self.__y <= self.__z
         cond2 = self.__z + self.__y <= self.__x
@@ -39,3 +61,34 @@ class TriangleT():
             return False
         else:
             return True
+
+    ## @brief Gets a TriType by checking with the provided sides
+    #  @return TriType for the provided sides
+    def tri_type(self):
+        if self.__y == self.__x == self.__z:
+            return TriType.equilat
+
+        if self.__x == self.__y or self.__y == self.__z or self.__z == self.__x:
+            return TriType.isosceles
+
+        a, b, c = self.__x ** 2, self.__y ** 2, self.__z ** 2
+        if max(a, b, c) == (a + b + c - max(a, b, c)):
+            return TriType.right
+
+        if self.__y != self.__x != self.__z:
+            return TriType.scalene
+
+## @brief TriType class is an enumeration of the different types of triangle
+class TriType(Enum):
+
+    ## enum value equilat
+    equilat = auto()
+
+    ## enum value isosceles
+    isosceles = auto()
+
+    ## enum value scalene
+    scalene = auto()
+
+    ## enum value right
+    right = auto()
