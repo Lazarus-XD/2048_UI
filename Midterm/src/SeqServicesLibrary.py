a3882b8c61d47d11faaaf378bc75cf0d0af6fa70
Rizwan Ahsan <rizahsan4@gmail.com>
2021-03-04 21:10:17 -0500

    added Q2 and Q3

--- Midterm/src/SeqServicesLibrary.py
+++ Midterm/src/SeqServicesLibrary.py
@@ -1,6 +1,33 @@
 ## @file SeqServicesLibrary.py
-#  @author Your name
+#  @author Rizwan Ahsan
 #  @brief Library module that provides functions for working with sequences
 #  @details This library assumes that all functions will be provided with arguments of the expected types
 #  @date 03/04/2021
 
+
+def max_val(s):
+    if len(s) == 0:
+        raise ValueError
+    return max(s)
+
+
+def count(t, s: list):
+    if len(s) == 0:
+        raise ValueError
+    return sum([1 for i in s if i == t])
+
+
+def spices(s):
+    if len(s) == 0:
+        raise ValueError
+    return ["nutmeg" if i <= 0 else "ginger" for i in s]
+
+
+def new_max_val(s, f):
+    if len(s) == 0:
+        raise ValueError
+    lst = []
+    for i in s:
+        if f:
+            lst.append(i)
+    return max_val(lst)
--- Midterm/src/SetOfInt.py
+++ Midterm/src/SetOfInt.py
@@ -1,4 +1,53 @@
 ## @file SetOfInt.py
-#  @author Your Name
+#  @author Rizwan Ahsan
 #  @brief Set of integers
 #  @date 03/04/2021
+
+
+class SetOfInt():
+
+    def __init__(self, x):
+        self.s = set()
+        for i in x:
+            self.s.add(i)
+
+    def is_member(self, x):
+        return x in self.s
+
+    def to_seq(self):
+        lst = []
+        for i in self.s:
+            lst.append(i)
+        return lst
+
+    def union(self, t):
+        lst1 = self.to_seq()
+        lst2 = t.to_seq()
+        lst = lst1 + lst2
+        return SetOfInt(lst)
+
+    def diff(self, t):
+        lst1 = self.to_seq()
+        lst2 = t.to_seq()
+        for i in lst2:
+            if i in lst1:
+                lst1.remove(i)
+        return SetOfInt(lst1)
+
+    def size(self):
+        return len(self.s)
+
+    def empty(self):
+        return len(self.s) == 0
+
+    def equals(self, t):
+        lst1 = t.to_seq()
+        lst2 = self.to_seq()
+        #sorts the lists in ascending order
+        lst2.sort()
+        lst1.sort()
+        #iterates over the lists to check if all items are equal
+        for i in range(len(lst1)):
+            if lst1[i] != lst2[i]:
+                return False
+        return True
