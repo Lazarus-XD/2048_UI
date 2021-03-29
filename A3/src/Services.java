85c092fdca04679d4ccb03513d276991f4604d0f
Rizwan Ahsan <ahsanm7@mcmaster.ca>
2021-03-29 17:30:09 -0400

    added Norm and Services

--- A3/src/Services.java
+++ A3/src/Services.java
@@ -1,1 +1,14 @@
 package src;
+
+public class Services {
+
+    public double[] normal(double[] v) {
+        double summ = 0;
+        double[] output = new double[v.length];
+        for (double i : v) summ += i;
+        for (int i = 0; i < v.length; i++) {
+            output[i] = v[i] / summ;
+        }
+        return output;
+    }
+}
