ee2bf895cf754ac8d4835ae24c6f12c2ba44ff68
Rizwan Ahsan <ahsanm7@mcmaster.ca>
2021-03-29 23:23:16 -0400

    added more test cases for CourseT

--- A3/src/TestCourseT.java
+++ A3/src/TestCourseT.java
@@ -41,4 +41,40 @@ public class TestCourseT
 //        String[] b = Arrays.toString(ind);
 
     }
+
+    @Test (expected = UnsupportedOperationException.class)
+    public void testMeasures()
+    {
+        c1.measures();
+    }
+
+    @Test
+    public void TestMeasures1() {
+        IndicatorT[] ind = new IndicatorT[] {IndicatorT.math, IndicatorT.specEngKnow, IndicatorT.assumpt};
+        CourseT c2 = new CourseT("Hello", ind);
+        LOsT LO1 = new LOsT("topic 1", 1, 2, 3, 4);
+        LOsT LO2 = new LOsT("topic 2", 1, 2, 3, 4);
+        LOsT LO3 = new LOsT("topic 3", 10, 20, 30, 40);
+        c2.addLO(IndicatorT.math, LO1);
+        c2.addLO(IndicatorT.math, LO2);
+        c2.addLO(IndicatorT.math, LO3);
+        assertTrue(Arrays.equals(c2.measures(IndicatorT.math), new double[]{12, 24, 36, 48}));
+    }
+
+    @Test
+    public void TestMeasures2() {
+        IndicatorT[] ind = new IndicatorT[] {IndicatorT.math, IndicatorT.specEngKnow, IndicatorT.assumpt};
+        AttributeT a = new AttributeT("HI", ind);
+        CourseT c2 = new CourseT("Hello", ind);
+        LOsT LO1 = new LOsT("topic 1", 2, 5, 5, 8);
+        LOsT LO2 = new LOsT("topic 2", 20, 15, 0, 2);
+        LOsT LO3 = new LOsT("topic 3", 2, 5, 5, 8);
+        c2.addLO(IndicatorT.math, LO1);
+        c2.addLO(IndicatorT.math, LO2);
+        c2.addLO(IndicatorT.math, LO3);
+        c2.addLO(IndicatorT.specEngKnow, LO2);
+        c2.addLO(IndicatorT.specEngKnow, LO3);
+        c2.addLO(IndicatorT.assumpt, LO1);
+        assertTrue(Arrays.equals(c2.measures(a), new double[]{48, 50, 20, 36}));
+    }
 }
