934d3574a5d96e2fc41d1cc08d34810d22a028f3
Rizwan Ahsan <ahsanm7@mcmaster.ca>
2021-03-29 22:33:48 -0400

    added test cases for AttributeT and LOsT modules

--- A3/src/TestAttributeT.java
+++ A3/src/TestAttributeT.java
@@ -1,22 +1,40 @@
 /**
- * Author: 
- * Revised: 
+ * Author: Rizwan Ahsan, ahsanm7
+ * Revised: March 29, 2021
  * 
- * Description: 
+ * Description: Tests the AttributT module
  */
 
 package src;
 
 import org.junit.*;
+import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class TestAttributeT
 {
 
+    private AttributeT att;
+
+    @Before
+    public void setUp() {
+        IndicatorT[] ind = new IndicatorT[] {IndicatorT.math, IndicatorT.specEngKnow, IndicatorT.assumpt};
+        att = new AttributeT("Students", ind);
+    }
+
+    @After
+    public void tearDown() {
+        att = null;
+    }
+
     @Test
-    public void testBlank()
-    {
-        assertTrue(true);
+    public void testgetName() {
+        assertTrue(att.getName().equals("Students"));
+    }
+
+    @Test
+    public void testgetIndicators() {
+        assertTrue(Arrays.equals(att.getIndicators(), new IndicatorT[]{IndicatorT.math, IndicatorT.specEngKnow, IndicatorT.assumpt}));
     }
 
 }
