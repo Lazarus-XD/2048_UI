1d9e57eaad8d8c24d6a67f312edc217eb9685eb5
Rizwan Ahsan <ahsanm7@mcmaster.ca>
2021-04-12 21:19:42 -0400

    added UserInterface and updated BoardT

--- A4/src/BoardT.java
+++ A4/src/BoardT.java
@@ -1,2 +1,80 @@
 package src;
 
+public class BoardT {
+
+    private int[][] board;
+    private boolean isOver;
+    private boolean isWon;
+
+    public BoardT(int size) {
+        this.board = new int[size][size];
+        this.isOver = false;
+    }
+
+    public void addNewNumber() {
+        int row = (int) (Math.random() * board.length);
+        int col = (int) (Math.random() * board.length);
+
+        while(board[row][col] != 0) {
+            row = (int) (Math.random() * board.length);
+            col = (int) (Math.random() * board.length);
+        }
+
+        if(Math.random() > 0.1) {
+            board[row][col] = 2;
+        } else {
+            board[row][col] = 4;
+        }
+    }
+
+    public void updateStatus() {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board.length; j++) {
+                if(board[i][j] == 2048) {
+                    isOver = true;
+                    isWon = true;
+                    return;
+                }
+                if(board[i][j] == 0) return;
+            }
+        }
+
+        for(int i = 0; i < board.length - 1; i++) {
+            for(int j = 0; j < board.length - 1; j++) {
+                if((board[i][j] == board[i + 1][j]) || (board[i][j] == board[i][j + 1]))
+                    return;
+            }
+        }
+
+        for(int i = 0; i < board.length - 1; i++) {
+            if(board[i][board.length - 1] == board[i + 1][board.length - 1]) return;
+        }
+
+        for(int j = 0; j < board.length - 1; j++) {
+            if(board[board.length - 1][j] == board[board.length - 1][j + 1]) return;
+        }
+
+        isOver = true;
+        isWon = false;
+    }
+
+    public int[][] transpose() {
+        int[][] result = new int[board.length][board.length];
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board.length; j++) {
+                result[i][j] = board[j][i];
+            }
+        }
+        return result;
+    }
+
+
+
+    public boolean getIsOver() {
+        return isOver;
+    }
+
+    public boolean getIsWon() {
+        return isWon;
+    }
+}
---
+++ A4/src/UserInterface.java
@@ -0,0 +1,51 @@
+package src;
+
+public class UserInterface {
+
+    public UserInterface(int[][] board) {
+        printBoard(board);
+    }
+
+
+    public void printWelcomeMessage() {
+        System.out.println("-------------------------------------------------");
+        System.out.println("                 Welcome to 2048                 ");
+        System.out.println("-------------------------------------------------");
+    }
+
+    public void printCommands() {
+        System.out.println("Commands to play the game:");
+        System.out.println("Up arrow: Move all tiles up");
+        System.out.println("Down arrow: Move all tiles down");
+        System.out.println("Left arrow: Move all tiles left");
+        System.out.println("Right arrow: Move all tiles right");
+    }
+
+    public void printLossMessage() {
+        System.out.println("-------------------------------------------------");
+        System.out.println("                   You lost :(          ");
+        System.out.println("             Thank You For Playing !!!           ");
+        System.out.println("-------------------------------------------------");
+    }
+
+    public void printWinMessage() {
+        System.out.println("-------------------------------------------------");
+        System.out.println("                   You won :)          ");
+        System.out.println("             Thank You For Playing !!!           ");
+        System.out.println("-------------------------------------------------");
+    }
+
+    public void printBoard(int[][] board) {
+        for (int[] ints : board) {
+            for (int j = 0; j < board.length; j++) {
+                if (ints[j] == 0) {
+                    System.out.print("|__");
+                } else {
+                    System.out.print("|" + ints[j]);
+                }
+            }
+            System.out.println("|");
+        }
+    }
+
+}
